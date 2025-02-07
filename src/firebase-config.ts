a1f039e3d19d60678eb100001749afc99aa8d946
hamawebdev <148373753+hamawebdev@users.noreply.github.com>
2025-02-07 23:53:37 +0100

    masked keys

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdDR6Q71RAYPLrjGbuqkS_R6_paUB506o",
-  authDomain: "docs-clone-93f77.firebaseapp.com",
-  projectId: "docs-clone-93f77",
-  storageBucket: "docs-clone-93f77.firebasestorage.app",
-  messagingSenderId: "18911280260",
-  appId: "1:18911280260:web:38c215ff481b44e7f4d6d1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
