2bae5f0be05d520b7e575517aba3bc7fcadee376
hamawebdev <148373753+hamawebdev@users.noreply.github.com>
2025-02-07 21:53:54 +0100

    editor first implementation

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,33 @@
 import React, { useRef } from 'react'
 import ReactQuill from 'react-quill'
+import {setDoc, doc} from 'firebase/firestore' 
+import {db} from 'firebase-config'
+
 
 const Editor = () => {
     const quillRef = useRef<any>(null);
+    const [isEditing, setIsEditing] = useState<boolean>(false);
 
+  const documentRef = doc(db, 'documents', 'test-doc');
+
+  const saveContent ():any =>{
+    if(quillRef.current){
+        const content = quillRef.current.getEditor().getContents();
+        console.log('saving to db...',content);
+
+    setDoc(documentRef, {content: content.ops}, merge: true)
+        .then(() => {console.log('content saved to db')})
+        .catch(console.error)
+    }
+  }
+
+  useEffect(() => {
+    if(quillRef.current){
+        quillRef.current.getEditor().on('text-change', () => {
+            setIsEditing(true);
+        })
+    }
+  }, [isEditing])
 
 
   return (
